33abf992c1aa9be5377db71bc2b128a8bc125fc5
macaoRabbit <seadragon8899@outlook.com>
2024-07-25 14:24:14 -0400

    Remove Data Files

--- src/main/java/com/tim/parser/CsvParser.java
+++ src/main/java/com/tim/parser/CsvParser.java
@@ -1,6 +1,15 @@
 package com.tim.parser;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CsvParser {
+    static final String COMMA_DELIMITER = ",";
     String fileName;
 
     public CsvParser(String fileName) {
@@ -13,5 +22,21 @@ public class CsvParser {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public List<List<String>> getRecords() {
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(getFileName()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return records;
     }
 }
--- src/test/java/com/tim/parser/CsvParserTest.java
+++ src/test/java/com/tim/parser/CsvParserTest.java
@@ -1,13 +1,19 @@
 package com.tim.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class CsvParserTest {
     @Test
-    public void fileNameTest () {
-        String fileName = "stockQuote.csv";
-        CsvParser p = new CsvParser(fileName);
-        assertTrue(fileName.equalsIgnoreCase(p.getFileName()));
+    public void fileNameTest() {
+        String dir = "C:\\GitHubProjects\\data\\";
+        String fileName = "AMD.csv";
+        CsvParser p = new CsvParser(dir + fileName);
+        List<List<String>> records = p.getRecords();
+        assertEquals(fileName, p.getFileName());
     }
 }
