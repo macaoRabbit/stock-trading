96719fea777bf2a0033ca5dd7e7066434d39dd53
macaoRabbit <seadragon8899@outlook.com>
2024-07-30 13:08:20 -0400

    Code Updates with new Tests

--- src/test/java/com/tim/trade/ControlTradeTest.java
+++ src/test/java/com/tim/trade/ControlTradeTest.java
@@ -2,24 +2,16 @@ package com.tim.trade;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ControlTradeTest {
     @Test
-    public void matchGroupQuoteMissingDatesTest() {
+    public void congtrolTradeAnalyzeTest() {
         String dir = "C:\\GitHubProjects\\data\\";
-        String f1 = "a1.csv";
-        String f2 = "a2.csv";
-        String f3 = "a3.csv";
-        List<String> files = new ArrayList<>();
-        files.add(dir + f1);
-        files.add(dir + f2);
-        files.add(dir + f3);
-        GroupQuoteMatcher g = new GroupQuoteMatcher(files);
-        List<GroupQuote> gQuotes = g.getMatchedQuotes();
-        assertTrue(gQuotes.size() > 0);
+        String f1 = "AMD.csv";
+        Float seedCost = 1000.0f;
+        ControlTrade t = new ControlTrade(dir + f1, seedCost);
+        t.analyze();
+        assertTrue(t.getAnnualizedReturn() > 0);
     }
 }
