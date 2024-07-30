b672123ba1ece777e6929deeba528b4f51c03f23
macaoRabbit <seadragon8899@outlook.com>
2024-07-30 13:04:47 -0400

    Code Updates with new Tests

--- src/test/java/com/tim/trade/GroupQuoteMatcherTest.java
+++ src/test/java/com/tim/trade/GroupQuoteMatcherTest.java
@@ -2,17 +2,24 @@ package com.tim.trade;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GroupQuoteMatcherTest {
     @Test
-    public void quoteMatcherTest() {
+    public void matchGroupQuoteMissingDatesTest() {
         String dir = "C:\\GitHubProjects\\data\\";
-        String f1 = "AMD.csv";
-        String f2 = "NOW.csv";
-        List<PairedQuotes> pairedQuotesList = new QuoteMatcher(dir + f1, dir + f2).generateMatchedQuotes();
-        assertTrue(pairedQuotesList.size() > 0);
+        String f1 = "a1.csv";
+        String f2 = "a2.csv";
+        String f3 = "a3.csv";
+        List<String> files = new ArrayList<>();
+        files.add(dir + f1);
+        files.add(dir + f2);
+        files.add(dir + f3);
+        GroupQuoteMatcher g = new GroupQuoteMatcher(files);
+        List<GroupQuote> gQuotes = g.getMatchedQuotes();
+        assertTrue(gQuotes.size() > 0);
     }
 }
