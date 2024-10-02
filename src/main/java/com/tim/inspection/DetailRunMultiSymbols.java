3679815647ecc5f34c8d45d761da101d2db9edd4
macaoRabbit <seadragon8899@outlook.com>
2024-10-02 17:49:47 -0400

    Code Updates with new Tests

---
+++ src/main/java/com/tim/inspection/DetailRunMultiSymbols.java
@@ -0,0 +1,167 @@
+package com.tim.inspection;
+
+import com.tim.experiment.FullPairExperiment;
+import com.tim.result.GroupTradeResult;
+import com.tim.result.GroupTradeResultItem;
+import com.tim.result.ReturnItemType;
+import com.tim.trade.*;
+import com.tim.utility.GapDetails;
+import com.tim.utility.Symbols;
+import com.tim.utility.TradingAlgorithm;
+import com.tim.utility.TradingHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailRunMultiSymbols {
+    static String dir = "C:\\GitHubProjects\\data\\";
+    static String resultDir = "C:\\GitHubProjects\\result\\";
+    //    static String stockList = "CEG,LLY";
+    static String stockList = "IJS,VHT,IVW";
+    static String date = Symbols.getStringDate();
+    static String fileAppendix = TradingHelper.FILE_TYPE;
+    static boolean includeSummary = true;
+    static boolean anyDayGap = true;
+    static Float gap = 0.05f;
+    static Float splitRatioPower = 0.05f;
+    static Integer tradeGroupSize = 2;
+    static TradingAlgorithm algorithm = TradingAlgorithm.PAIR_SWAP;
+    static String resultFile = algorithm.name() + "_";
+
+    public static void main(String[] args) {
+        run(algorithm, stockList, gap, splitRatioPower);
+    }
+
+    public static void run(TradingAlgorithm algorithm, String stockList, Float gap, Float splitRatioPower) {
+        DetailRunMultiSymbols.algorithm = algorithm;
+        DetailRunMultiSymbols.stockList = stockList;
+        DetailRunMultiSymbols.gap = gap;
+        DetailRunMultiSymbols.splitRatioPower = splitRatioPower;
+        Float seedCost = 1000.0f;
+        String stocks[] = stockList.split(",");
+        tradeGroupSize = stocks.length;
+        boolean lossMajor = true;
+
+        for (int index = 0; index < tradeGroupSize; index++) {
+            List<Trading> tradings = new ArrayList<>();
+            for (int i = 0; i < getTradeGroupSize(); i++) {
+                String filiName = stocks[i].trim() + fileAppendix;
+                Trading t = new GapTrading(dir + filiName, 0.0f);
+                if (i == index) {
+                    t = new GapTrading(dir + filiName, seedCost);
+                }
+                tradings.add(t);
+            }
+            runJustOne(tradings, index, gap, lossMajor, includeSummary, anyDayGap);
+        }
+    }
+    private static void runJustOne(List<Trading> tradings, int index, Float gap, boolean lossMajor, boolean includeSummary, boolean anyDayGap) {
+        String num = "_" + String.format("%1d", index) + "_";
+        GroupTradeDayGapRatioTrading g = TradingAlgorithm.getAlgorithm(algorithm);
+        g.setGapSize(gap);
+        g.setSplitRatioPower(splitRatioPower);
+        g.setLossMajor(lossMajor);
+        g.setAnyDayGap(anyDayGap);
+        g.getTradings().addAll(tradings);
+        Float controlReturn = FullPairExperiment.getThisControlReturn(g, index);
+        g.setControlReturn(controlReturn);
+        g.initQuotesWithCsvFileForAllTradings();
+        g.matchQuotesForAllTradings();
+        g.analyze();
+        g.report();
+        List<GroupTradeResult> results = new ArrayList<>();
+        if (includeSummary) {
+            results.add(g.collectResult());
+            creatSymbolList(g, results);
+        }
+        creatDailyResults(g, results);
+        String summary = "";
+        if (includeSummary) {
+            summary = "_sum";
+        }
+        String anyDay = "_tradeD";
+        if (anyDayGap) {
+            anyDay = "_anyD";
+
+        }
+        String pGap = String.format("_G%03.0f", gap * 1000);
+        String subDir = resultDir + g.getSymbolList().trim();
+        String lossOrNot = "_" + String.format("%b", lossMajor);
+        String saveFile = subDir + "\\" + resultFile + g.getSymbolList().trim() + pGap + anyDay + lossOrNot + summary + num + date + fileAppendix;
+        new GroupTradeResult().save(results, subDir, saveFile, false);
+    }
+
+    private static void creatSymbolList(GroupTradeDayGapRatioTrading g, List<GroupTradeResult> results) {
+        GroupTradeResult r = new GroupTradeResult();
+        for (Trading t : g.getTradings()) {
+            GroupTradeResultItem d = new GroupTradeResultItem("symbols", String.format("%12s", t.getSymbol()), ReturnItemType.StringType);
+            r.getResults().add(d);
+        }
+        results.add(r);
+    }
+
+    private static void creatDailyResults(GroupTradeDayGapRatioTrading g, List<GroupTradeResult> results) {
+        int days = g.getTradings().get(0).getTrades().size();
+        for (int i = 0; i < days; i++) {
+            GroupTradeResult r = new GroupTradeResult();
+            addDate(g, i, r);
+            addSharesAndEquity(g, i, r);
+            addGapDetails(g, i, r);
+            results.add(r);
+        }
+    }
+
+    private static void addSharesAndEquity(GroupTradeDayGapRatioTrading g, int i, GroupTradeResult r) {
+        Float cEquity = 0.0f;
+        Float gEquity = 0.0f;
+        for (Trading t : g.getTradings()) {
+            Trade trade = t.getTrades().get(i);
+            Float controlShares = t.getTrades().get(0).getShares();
+            GroupTradeResultItem i1 = new GroupTradeResultItem(t.getSymbol() + "_price", String.format("%7.2f", trade.getSharePrice()), ReturnItemType.FloatType);
+            GroupTradeResultItem i2 = new GroupTradeResultItem(t.getSymbol() + "_shares", String.format("%7.2f", trade.getShares()), ReturnItemType.FloatType);
+            Float equity = trade.getSharePrice() * trade.getShares();
+            GroupTradeResultItem i3 = new GroupTradeResultItem(t.getSymbol() + "_equity", String.format("%9.2f", equity), ReturnItemType.FloatType);
+            r.getResults().add(i1);
+            r.getResults().add(i2);
+            r.getResults().add(i3);
+            gEquity = gEquity + equity;
+            cEquity = cEquity + trade.getSharePrice() * controlShares;
+        }
+        GroupTradeResultItem i1 = new GroupTradeResultItem("cEquity", String.format("%9.2f", cEquity), ReturnItemType.FloatType);
+        GroupTradeResultItem i2 = new GroupTradeResultItem("gEquity", String.format("%9.2f", gEquity), ReturnItemType.FloatType);
+        r.getResults().add(i1);
+        r.getResults().add(i2);
+    }
+
+    private static void addDate(GroupTradeDayGapRatioTrading g, int i, GroupTradeResult r) {
+        String date = g.getTradings().get(0).getTrades().get(i).getStringDay();
+        GroupTradeResultItem d = new GroupTradeResultItem("date", String.format("%12s", date), ReturnItemType.StringType);
+        r.getResults().add(d);
+    }
+
+    private static void addGapDetails(GroupTradeDayGapRatioTrading g, int i, GroupTradeResult r) {
+        if (i >= g.getDailyGaps().size()) return;
+        GapDetails d = g.getDailyGaps().get(i);
+        Double min = d.getMins().stream().mapToDouble(x -> x.getRatio()).min().orElse(-1.0f);
+        Double max = d.getMaxs().stream().mapToDouble(x -> x.getRatio()).max().orElse(-1.0f);
+        GroupTradeResultItem i1 = new GroupTradeResultItem("gap", String.format("%9.3f", d.getGap()), ReturnItemType.FloatType);
+        GroupTradeResultItem i2 = new GroupTradeResultItem("min", String.format("%9.3f", min), ReturnItemType.FloatType);
+        GroupTradeResultItem i3 = new GroupTradeResultItem("max", String.format("%9.3f", max), ReturnItemType.FloatType);
+        GroupTradeResultItem i4 = new GroupTradeResultItem("date", String.format("%12s", d.getGapDay()), ReturnItemType.StringType);
+        GroupTradeResultItem i5 = new GroupTradeResultItem("duration", String.format("%6d", d.getDuration()), ReturnItemType.IntegerType);
+        r.getResults().add(i1);
+        r.getResults().add(i2);
+        r.getResults().add(i3);
+        r.getResults().add(i4);
+        r.getResults().add(i5);
+    }
+
+    public static Integer getTradeGroupSize() {
+        return tradeGroupSize;
+    }
+
+    public static void setTradeGroupSize(Integer tradeGroupSize) {
+        PairSwapMultiSymbols.tradeGroupSize = tradeGroupSize;
+    }
+}
+
---
+++ src/main/java/com/tim/inspection/DetailRunOnce.java
@@ -0,0 +1,15 @@
+package com.tim.inspection;
+
+import com.tim.utility.TradingAlgorithm;
+
+public class DetailRunOnce {
+    //    static String stockList = "CEG,LLY";
+    static String stockList = "IJS,VHT,IVW";
+    static Float gap = 0.05f;
+    static Float splitRatioPower = 0.05f;
+    static TradingAlgorithm algorithm = TradingAlgorithm.PAIR_SWAP;
+
+    public static void main(String[] args) {
+        DetailRunMultiSymbols.run(algorithm, stockList, gap, splitRatioPower);
+    }
+}
